d0b77dae5a22163b909b166e0b43b4e9ef260d34
chenzhixin031020 <chenzhixin456@gmail.com>
2023-11-07 14:47:48 +0800

    feat: Ignore

--- chatbot-api-interfaces/src/test/java/cn/zack/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/zack/chatbot/api/test/ApiTest.java
@@ -1,9 +1,63 @@
 package cn.zack.chatbot.api.test;
-/**
- * @author 小傅哥，微信：fustack
- * @description 单元测试
- * @github https://github.com/fuzhengwei
- * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- */
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+@Test
 public class ApiTest {
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/88885181248242/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie","sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2218b9d3d350ce0b-091242b8b69b37-745d5777-1327104-18b9d3d350d160e%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMThiOWQzZDM1MGNlMGItMDkxMjQyYjhiNjliMzctNzQ1ZDU3NzctMTMyNzEwNC0xOGI5ZDNkMzUwZDE2MGUifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%2218b9d3d350ce0b-091242b8b69b37-745d5777-1327104-18b9d3d350d160e%22%7D; abtest_env=product; zsxqsessionid=c8a117335273db92e87e93baec77d0b0; zsxq_access_token=7E428EE1-EFF9-8866-B599-DC4CCF59FC40_7CE4A151C8AB4C42");
+        get.addHeader("content-type","application/json; charset=UTF-8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else{
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/411528222888558/answer");
+        post.addHeader("cookie","sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%2218b9d3d350ce0b-091242b8b69b37-745d5777-1327104-18b9d3d350d160e%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMThiOWQzZDM1MGNlMGItMDkxMjQyYjhiNjliMzctNzQ1ZDU3NzctMTMyNzEwNC0xOGI5ZDNkMzUwZDE2MGUifQ%3D%3D%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%22%2C%22value%22%3A%22%22%7D%2C%22%24device_id%22%3A%2218b9d3d350ce0b-091242b8b69b37-745d5777-1327104-18b9d3d350d160e%22%7D; abtest_env=product; zsxqsessionid=c8a117335273db92e87e93baec77d0b0; zsxq_access_token=7E428EE1-EFF9-8866-B599-DC4CCF59FC40_7CE4A151C8AB4C42");
+        post.addHeader("content-type","application/json; charset=UTF-8");
+
+
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+    }
 }
